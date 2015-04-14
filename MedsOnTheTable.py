69279c5ce262700ea25c2c47b5f5c8d9706d84c0
AlexTelon <alex.telon@gmail.com>
2015-04-14 15:47:54 +0200

    added small test for opening sites with different info on for different meds

--- MedsOnTheTable.py
+++ MedsOnTheTable.py
@@ -7,5 +7,10 @@ def index():
     return render_template("layout.html")
     #return 'Hello World!'
 
+@app.route('/med/<brand>')
+def brandInfo(brand):
+    return 'Medecinen: %s' % brand;
+
+
 if __name__ == '__main__':
     app.run(debug=True)
