327022689163d7bf6bba2ce461b4bb19417ed3db
Alex Telon <alex.telon@gmail.com>
2015-04-26 13:02:04 -0700

    fixed ascii issue and have some preliminary tests with the SIL server

--- MedsOnTheTable.py
+++ MedsOnTheTable.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
+import suds
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("layout.html")
+    url = "http://sil40.test.silinfo.se/silapi40/SilDB?wsdl"
+    sil = suds.client.Client (url)
+    ddrugs = sil.service.getDistributedDrugsByDistributedDrugTradeName ("Aspirin", True, -1)
+
+    print ddrugs
+    return render_template("test.html", info=ddrugs)
     #return 'Hello World!'
+
+@app.route('/s')
+def search():
+    url = "http://sil40.test.silinfo.se/silapi40/SilDB?wsdl"
+    sil = suds.client.Client (url)
+    subs = sil.service.getSubstancesBySubstanceName ("ace%")
+    print subs
+    return render_template("test.html", info=subs)
+    #return 'Hello World!'
+
+
+
 
 @app.route('/med/<brand>')
 def brandInfo(brand):
