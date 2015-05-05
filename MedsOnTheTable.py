95c52031c340318da272b2810fa8e9d6de1a5b31
johanordin <johno855@student.liu.se>
2015-05-05 21:08:41 +0200

    tested on nexus

--- MedsOnTheTable.py
+++ MedsOnTheTable.py
@@ -1,12 +1,15 @@
+import sys
+
 from flask import Flask, render_template
 import suds
-import sys
+
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
 app = Flask(__name__)
 medArray = []
+
 
 @app.route('/')
 def index():
@@ -16,11 +19,11 @@ def index():
 @app.route('/search')
 def search():
     url = "http://sil40.test.silinfo.se/silapi40/SilDB?wsdl"
-    sil = suds.client.Client (url)
-    #subs = sil.service.getSubstancesBySubstanceName ("Kodein%")
+    sil = suds.client.Client(url)
+    # subs = sil.service.getSubstancesBySubstanceName ("Kodein%")
     #subs = sil.service.getDistributedDrugsByDistributedDrugTradeName ("Citodon", False, -1)
     #subs = sil.service.getDrugsByAtcCode ("N02AA59", False, -1)
-    subs = sil.service.getDrugArticlesByDrugId ("20090916000021", False, -1)
+    subs = sil.service.getDrugArticlesByDrugId("20090916000021", False, -1)
     #subs = sil.service.getSILPregnancyLactationWarningsByNplIdList ("20090916000021")
     #subs = sil.service.getSuperDrugsByDistributedDrugTradeName("Aspirin", False, -1)
     #return subs[0]['text']
@@ -31,12 +34,12 @@ def search():
 @app.route('/info/<id>')
 def info(id):
     url = "http://sil40.test.silinfo.se/silapi40/SilDB?wsdl"
-    sil = suds.client.Client (url)
+    sil = suds.client.Client(url)
     Fass = sil.service.getFassDocsByDrugId(str(id))
     return Fass[0]['XHtml']
 
 
-#ATC is about what kind of medecine we are talking about, Different groups like blood and nervous system.
+# ATC is about what kind of medecine we are talking about, Different groups like blood and nervous system.
 
 # How we get stuff from a ddrug ddrug[0]['nplId'].
 
@@ -54,6 +57,7 @@ def brandInfo(brand):
     return render_template('fassTest.html', ids=medArray)
     #return 'Medecinen: %s' % brand;
 
+
 @app.route('/clearallids')
 def clearAllIds():
     while 0 != len(medArray):
@@ -63,8 +67,16 @@ def clearAllIds():
 
 @app.route('/card')
 def card_view():
-    return render_template('card_view.html')
+    return render_template('card_view.html', ids=medArray)
 
+@app.route('/grid')
+def grid():
+    return render_template('grid_system.html')
+
+
+@app.route('/drug_info')
+def drug_info():
+    return render_template('drug_info.html')
 
 
 
